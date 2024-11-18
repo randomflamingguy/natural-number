f964d4e7288de2055456606a8906b6f33f565c6f
randomflamingguy <randomflamingguy1@gmail.com>
2024-11-18 17:37:32 +0600

    nsturalnunnkfgrtt

---
+++ main.py
@@ -0,0 +1,7 @@
+def natural():
+    num = int(input("Please enter a number"))
+    if num > 0:
+        print("The number is a natural number")
+    else:
+        print("The number is not a natural number")
+natural()
